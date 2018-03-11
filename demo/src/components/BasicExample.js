58e019b8281bd2375c5995e4fabe61d67e7e7b65
Eric Betts <bettse@fastmail.fm>
2018-03-10 16:48:45 -0800

    Move layout into its own file

--- demo/src/components/BasicExample.js
+++ demo/src/components/BasicExample.js
@@ -9,49 +9,7 @@ import FlatButton from "material-ui/FlatButton";
 
 import { Link } from "react-router-dom";
 import Highlight from "react-highlight";
-
-let data = {
-  numPanels: 6,
-  sideLength: 150,
-  positionData: [
-    {
-      panelId: 1,
-      x: 100,
-      y: 100,
-      o: 0
-    },
-    {
-      panelId: 200,
-      x: 25,
-      y: -29,
-      o: 240
-    },
-    {
-      panelId: 30,
-      x: 174,
-      y: -29,
-      o: 120
-    },
-    {
-      panelId: 4,
-      x: 99,
-      y: 13,
-      o: 180
-    },
-    {
-      panelId: 50,
-      x: 99,
-      y: -159,
-      o: 0
-    },
-    {
-      panelId: 600,
-      x: 25,
-      y: -116,
-      o: 60
-    }
-  ]
-};
+import * as data from '../basic_layout.json';
 
 export default class BasicExample extends Component {
   render() {
@@ -71,49 +29,7 @@ export default class BasicExample extends Component {
                 {`
     import React, {Component} from 'react';
     import NanoleafLayout from "../../../src/index";
-
-    let data = {
-        numPanels: 6,
-        sideLength: 150,
-        positionData: [
-            {
-                panelId: 1,
-                x: 100,
-                y: 100,
-                o: 0,
-            },
-            {
-                panelId: 200,
-                x: 25,
-                y: -29,
-                o: 240,
-            },
-            {
-                panelId: 30,
-                x: 174,
-                y: -29,
-                o: 120,
-            },
-            {
-                panelId: 4,
-                x: 99,
-                y: 13,
-                o: 180,
-            },
-            {
-                panelId: 50,
-                x: 99,
-                y: -159,
-                o: 0,
-            },
-            {
-                panelId: 600,
-                x: 25,
-                y: -116,
-                o: 60,
-            }
-        ]
-    };
+    import * as data from '../basic_layout.json';
 
     export default class BasicExample extends Component {
 
