70f8213aff44823431398349f08bb78be49c8770
Eric Betts <bettse@fastmail.fm>
2020-03-08 15:44:37 -0700

    Add endcaps

--- src/utils.js
+++ src/utils.js
@@ -71,6 +71,33 @@ export const colorAsInt = (hexString) => {
 };
 
 /**
+ * Returns a <g> with three smaller 'endCap' triangles
+ * @param props
+ * @returns <g>
+ */
+export const endCaps = (panel, sideLength) => {
+  const fill = '#ffffff'
+  const capSize = sideLength * 0.15
+  const points = equilateral(sideLength - capSize)
+  const {path} = draw([panel], capSize).pop()
+
+  return (
+    <g>
+      <g key='top_cap' transform={`translate(${points.topVertex[0]},${points.topVertex[1]})`} >
+        <path d={path} fill={fill} stroke={fill} strokeWidth={2} />
+      </g>
+      <g key='left_cap' transform={`translate(${points.leftVertex[0]},${points.leftVertex[1]})`} >
+        <path d={path} fill={fill} stroke={fill} strokeWidth={2} />
+      </g>
+      <g key='right_cap' transform={`translate(${points.rightVertex[0]},${points.rightVertex[1]})`} >
+        <path d={path} fill={fill} stroke={fill} strokeWidth={2} />
+      </g>
+    </g>
+  )
+}
+
+
+/**
  * Handles recalculating values and updating when the layout changes
  * @returns {Array}
  */
@@ -103,6 +130,7 @@ export const update = (props) => {
                     fill={value.color || color}
                     stroke={value.strokeColor || strokeColor}
                 />
+                {endCaps(value, props.data.sideLength)}
                 {
                     showCenter && <circle key={key + '_circle'} cx={0} cy={0} r={5} fill={'pink'}/>
                 }
